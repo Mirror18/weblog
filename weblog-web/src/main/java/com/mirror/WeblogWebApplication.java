8d657f6fc81f6d763a714fa31670ca8f18531fca
mirror <mirrorcloud3@gmail.com>
2024-10-09 00:10:48 +0800

    建立初始文件夹

---
+++ weblog-web/src/main/java/com/mirror/WeblogWebApplication.java
@@ -0,0 +1,14 @@
+package com.mirror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author mirror
+ */
+@SpringBootApplication
+public class WeblogWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WeblogWebApplication.class,args);
+    }
+}
