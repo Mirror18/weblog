81b4946269ec0c276c161e5d1a94ed04be3bdabc
mirror <mirrorcloud3@gmail.com>
2024-10-11 03:49:30 +0800

    实现参数校验

--- weblog-web/src/main/java/com/mirror/weblog/web/controller/TestController.java
+++ weblog-web/src/main/java/com/mirror/weblog/web/controller/TestController.java
@@ -3,10 +3,17 @@ package com.mirror.weblog.web.controller;
 import com.mirror.weblog.common.aspect.ApiOperationLog;
 import com.mirror.weblog.web.model.User;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError;
+
+import java.util.stream.Collectors;
 
 /**
  * @author mirror
@@ -17,8 +24,25 @@ public class TestController {
 
     @PostMapping("/test")
     @ApiOperationLog(description = "测试接口")
-    public User test(@RequestBody User user) {
+//    public User test(@RequestBody User user) {
+//        // 返参
+//        return user;
+//    }
+    //@Validated是告诉spring 需要对User对象执行校验
+    //BindingResult 验证的结果对象，其中包含所有验证错误信息
+    public ResponseEntity<String> test(@RequestBody @Validated User user, BindingResult bindingResult) {
+        // 是否存在校验错误
+        if (bindingResult.hasErrors()) {
+            // 获取校验不通过字段的提示信息
+            String errorMsg = bindingResult.getFieldErrors()
+                    .stream()
+                    .map(FieldError::getDefaultMessage)
+                    .collect(Collectors.joining(", "));
+
+            return ResponseEntity.badRequest().body(errorMsg);
+        }
+
         // 返参
-        return user;
+        return ResponseEntity.ok("参数没有任何问题");
     }
 }
--- weblog-web/src/main/java/com/mirror/weblog/web/model/User.java
+++ weblog-web/src/main/java/com/mirror/weblog/web/model/User.java
@@ -1,5 +1,7 @@
 package com.mirror.weblog.web.model;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 /**
@@ -9,7 +11,19 @@ import lombok.Data;
 public class User {
 
     // 用户名
+    //注解确保用户名不为空
+    @NotBlank(message = "用户名不能为空")
     private String username;
     // 性别
+    @NotNull(message = "性别不能为空")
     private Integer sex;
+
+    @NotNull(message = "年龄不能为空")
+    @Min(value = 18, message = "年龄必须大于或者等于18")
+    @Max(value = 100, message = "年龄必须小于或等于100")
+    private Integer age;
+
+    @NotBlank(message = "邮箱不能为空")
+    @Email(message = "邮箱格式不正确")
+    private String email;
 }
